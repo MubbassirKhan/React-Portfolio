697477841abe62f5fc3c088e5048115e09b802cc
Mubbassir Khan Jahagirdar <khanmkj96@gmail.com>
2026-05-25 13:16:33 +0530

    Feat: Images updated

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Mubbassir Khan A Jahagirdar",
-  profile: '/profile.png',
+  profile: '/Khan_pic.png',
   designation: "Backend Developer | Python Developer | Full-Stack Developer",
   description: "Results-driven Full-Stack Developer with extensive hands-on experience in building robust web applications using React, Python (FastAPI & Flask), MongoDB, and PostgreSQL. Possesses a solid foundation in machine learning through internships and academic projects. Strong passion for backend architecture and modern web technologies drives the pursuit of innovative solutions. Actively exploring application development with cutting-edge AI tools such as GitHub Copilot, ChatGPT, and Gemini to enhance productivity and creativity. Committed to contributing technical expertise while continuously upgrading skills in a dynamic organization that values growth and innovation.",
   email: 'mubbassirkhan96@gmail.com',
